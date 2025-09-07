c42e6a43a1a9a54e94f02932910d16bc6ab5c174
faizansaiyed123 <fai1ggj@gmail.com>
2025-09-07 11:58:30 +0530

    Register Model Class to Show in Admin Panel Django

---
+++ ch13/student/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Profile(models.Model):
+    name = models.CharField(max_length=20)
+    roll = models.IntegerField()
+    email = models.EmailField(max_length=30)
+    city = models.CharField(max_length=30)
+
+    def __str__(self):
+        return str(self.name)
+    
